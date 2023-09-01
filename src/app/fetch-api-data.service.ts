7d10b47a3d8276e5fc50ac56a9f494de2694600d
danielcli-dev <98434650+danielcli-dev@users.noreply.github.com>
2023-08-31 17:25:11 -0700

    fixed addFavoriteMovie service

--- src/app/fetch-api-data.service.ts
+++ src/app/fetch-api-data.service.ts
@@ -103,15 +103,13 @@ export class FetchApiDataService {
   addFavoriteMovie(name: string, id: string): Observable<any> {
     const token = localStorage.getItem('token');
     return this.http
-      .get(apiUrl + 'users/' + name, {
+      .get(apiUrl + 'users/' + name + '/add/' + id, {
         headers: new HttpHeaders({
           Authorization: 'Bearer ' + token,
         }),
       })
       .pipe(map(this.extractResponseData), catchError(this.handleError));
   }
-
-
 
   // addFavoriteMovie(name: string, id: string): Observable<any> {
   //   const token = localStorage.getItem('token');
